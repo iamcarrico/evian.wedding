71468983d0816c0aba5b5b1b88b408a038f5af11
Ian Carrico <git@iancarrico.com>
2018-04-11 16:45:12 -0500

    that is part of the quite as well

--- src/components/Reading.vue
+++ src/components/Reading.vue
@@ -14,9 +14,9 @@
       <p>When two people fall in love, each comes in out of the loneliness of exile, home to the one house of belonging. At weddings, it is appropriate to acknowledge the gracious destiny that enabled this couple to recognize each other when they met. Each recognized the other as the one in whom their heart could be at home… To be holy is to be home, to be able to rest in the house of belonging that we call the soul. Love is the nature of the soul. When we love and allow our selves to be loved, we begin more and more to inhabit the kingdom of the eternal.</p>
 
       <p>The greatest gift new love brings into your life is the awakening to the hidden love within. A person should always offer a prayer of graciousness for the love that has awakened in them. When you feel love for your beloved and his love for you, now and again you should offer the warmth of your love as a blessing for those who are damaged and unloved. Send that love out into the world to people who are desperate; to those who are starving; to those who are trapped in prison; in hospitals and all the brutal terrains of bleak and tormented lives. When you send that love out from the bountifulness of your own love, it reaches other people.</p>
-    </blockquote>
 
-    <p>There is a lovely idea in the Celtic tradition that if you send out goodness from your self, or if you share that which is happy or good within you, it will all come back to you multiplied ten thousand times. In the kingdom of love there is no competition; there is no possessiveness or control. The more love you give away, the more love you will have.</p>
+      <p>There is a lovely idea in the Celtic tradition that if you send out goodness from your self, or if you share that which is happy or good within you, it will all come back to you multiplied ten thousand times. In the kingdom of love there is no competition; there is no possessiveness or control. The more love you give away, the more love you will have.</p>
+    </blockquote>
 
     <p>Ian and Evan, may you both be blessed by love and may your love be a blessing to all the world.</p>
 
