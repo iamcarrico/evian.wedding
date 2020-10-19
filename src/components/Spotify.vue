65afb44563e87327087dbfde5a05d0868bb94da4
Ian Carrico <git@iancarrico.com>
2020-10-19 13:16:03 -0400

    Update link because of the user change

--- src/components/Spotify.vue
+++ src/components/Spotify.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="m-spotify">
-    <iframe src="https://open.spotify.com/embed/user/chinggizkhan/playlist/2157BVVV7uCn5sZc9qDqW5" width="375" height="380" frameborder="0" allowtransparency="true"></iframe>
+    <iframe src="https://open.spotify.com/embed/playlist/2157BVVV7uCn5sZc9qDqW5" width="375" height="380" frameborder="0" allowtransparency="true"></iframe>
 
   </div>
 </template>
